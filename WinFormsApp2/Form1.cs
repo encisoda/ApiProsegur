339dbf4b78b324dac03e244335dd7e9ba8549412
encisoda <encisoda@gmail.com>
2021-12-02 17:33:45 -0300

    api prosegur

---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace WinFormsApp2
+{
+	public partial class frmPrincipal : Form
+	{
+		public frmPrincipal()
+		{
+			InitializeComponent();
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+
+	public class Clima
+	{
+
+		static HttpClient client = new HttpClient();
+		private const string URL = "api.openweathermap.org/data/2.5/weather?q=";
+		private string APIkey = "9df32da903b50f1451dd73ec93f04cc6";
+
+
+	}
+
+
+}
